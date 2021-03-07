728cee5860c26971ca8120fd4e0351517a035dcc
Tabithamutinda <tabbithamutinda@gmail.com>
2021-03-07 15:28:18 +0300

    fruits

---
+++ index.js
@@ -0,0 +1,9 @@
+function fruits(){ 
+    fruits_list=['Pineapples','Watermelon','Apples','Oranges','Mangoes'];
+    fruits_list.forEach(element=> console.log(element));
+    console.log(fruits_list[0]);
+
+}
+fruits();
+    
+   
